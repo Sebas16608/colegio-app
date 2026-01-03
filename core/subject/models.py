3659517db00beb3a9ec28e48b05ca63f3adcbc81
Sebas16608 <asebasrr444@gmail.com>
2026-01-03 13:27:50 -0600

    Definiendo los modelos de subject

--- core/subject/models.py
+++ core/subject/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from user.models import User
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subject")
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Subject"
+        verbose_name_plural = "Subjects"
+        unique_together = ("teacher", "name")
+
+    def __str__(self):
+        return f"Subject {self.name} by {self.teacher.username}"
