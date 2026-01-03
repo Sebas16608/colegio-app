b3a20050cd33f59babbdfc960a3c28915eb3cf12
Sebas16608 <asebasrr444@gmail.com>
2026-01-03 11:36:23 -0600

    Definiendo los models de User

--- core/user/models.py
+++ core/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ("STUDENT", "Student"),
+        ("TEACHER", "Teacher"),
+        ("ADMIN", "Admin")
+    ]
+    role = models.CharField(max_length=25, choices=ROLE_CHOICES, default="STUDENT")
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
