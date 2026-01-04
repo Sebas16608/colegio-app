d191ece5aad73a958c869085ea4e96f28d271efb
Sebas16608 <asebasrr444@gmail.com>
2026-01-03 18:06:30 -0600

    Definiendo los modelos de enrollment

--- core/enrollment/models.py
+++ core/enrollment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from user.models import User
+from subject.models import Subject
 # Create your models here.
+class Enrollment(models.Model):
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="enrollment")
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    creatred_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Enrollment"
+        verbose_name_plural = "Enrollments"
+
+    def __str__(self):
+        return f"{self.student} ({self.subject})"
