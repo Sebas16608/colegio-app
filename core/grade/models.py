bd20f3d17ce10d1ca09634f47b1a86b675372943
Sebas16608 <asebasrr444@gmail.com>
2026-01-05 22:11:00 -0600

    definiendo los modelos para las notas

--- core/grade/models.py
+++ core/grade/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from submission.models import Submission
 # Create your models here.
+class Grade(models.Model):
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE, related_name="grades")
+    grade = models.PositiveIntegerField()
+    feedback = models.TextField(blank=True, null=True)
+    graded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Grade"
+        verbose_name_plural = "Grades"
+
+    def __str__(self):
+        return f"Grade {self.grade} for {self.submission.assignment.title} by {self.submission.student.username}"
