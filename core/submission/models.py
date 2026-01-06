05c441da927442f3ba699d938bfce9e125191879
Sebas16608 <asebasrr444@gmail.com>
2026-01-05 19:40:30 -0600

    Creando los models para subimissions para que los alumnos puedan subir tareas2

--- core/submission/models.py
+++ core/submission/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from assignment.models import Assignment
+from user.models import User
 
-# Create your models here.
+class Submission(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE, related_name="submissions")
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="submissions")
+    file_url = models.URLField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
+        verbose_name = "Submission"
+        verbose_name_plural = "Submissions"
+
+    def __str__(self):
+        return f"{self.assignment.title} by {self.student.username}"
