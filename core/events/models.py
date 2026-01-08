8629108ce5e3b1dcdd182d64b6965779aca24548
Sebas16608 <asebasrr444@gmail.com>
2026-01-08 09:11:51 -0600

    Creando los models para los events o calendar

--- core/events/models.py
+++ core/events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from user.models import User
 # Create your models here.
+class Events(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name=("events"))
+    
+    class Meta:
+        ordering = ["start_date"]
+        verbose_name = "Event"
+        verbose_name_plural = "Events"
+
+    def __str__(self):
+        return self.title
