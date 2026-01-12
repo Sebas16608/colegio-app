f3f3fe57d7547615af3662f1e253cc17b5d3db45
Sebas16608 <asebasrr444@gmail.com>
2026-01-12 15:42:21 -0600

    Programando la API con los filtros para buscar (creo que agregare el apartado de slug) y programando las urls

---
+++ core/assignment/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import AssignmentView
+
+urlpatterns = [
+    path("/", AssignmentView.as_view(), name="assignment-list"),
+    path("/<int:id>/")
+]
--- core/assignment/views.py
+++ core/assignment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from API import SuperApiView
+from .models import Assignment
+from .serializers import AssigmentSerializer
 # Create your views here.
+class AssignmentView(SuperApiView):
+    model = Assignment
+    serializer_class = AssigmentSerializer
+    filter_fields = ["title"]
