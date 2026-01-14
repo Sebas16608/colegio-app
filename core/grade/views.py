260a0d1c013c78d84e2f292d1d0c2ef1c0ea7f38
Sebas16608 <asebasrr444@gmail.com>
2026-01-13 22:01:33 -0600

    Definiendo las vistas para grade

--- core/grade/views.py
+++ core/grade/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from API import SuperApiView
+from .models import Grade
+from .serializer import GradeSerializer
 # Create your views here.
+class GradeView(SuperApiView):
+    model = Grade
+    serializer_class = GradeSerializer
+    filter_fields = ["grade", "id"]
