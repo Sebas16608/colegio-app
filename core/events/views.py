4336c214d54b857c32e6897d93f895c475243d78
Sebas16608 <asebasrr444@gmail.com>
2026-01-13 21:41:33 -0600

    Definiendo la API de events

--- core/events/views.py
+++ core/events/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from API import SuperApiView
+from .models import Events
+from .serializers import EventSerializer
 # Create your views here.
+class EventView(SuperApiView):
+    model = Events
+    serializer_class = EventSerializer
+    filter_fields = ["title", "id"]
