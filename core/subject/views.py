90ab0182d13afcc9e570a4ef9bc1eeafc6a611cd
Sebas16608 <asebasrr444@gmail.com>
2026-01-15 19:28:22 -0600

    Definiendo la API y los campos para hacer request

--- core/subject/views.py
+++ core/subject/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from API import SuperApiView
+from .serializers import SubjectSerializer
+from .models import Subject
 # Create your views here.
+class SubjectView(SuperApiView):
+    model = Subject
+    serializer_class = SubjectSerializer
+    filter_fields = ["name", "id"]
