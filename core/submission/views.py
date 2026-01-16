057c8acd25063c5ecb6bddbf220bb0746627fb48
Sebas16608 <asebasrr444@gmail.com>
2026-01-15 22:29:43 -0600

    definiendo la API con los datos para filtrar modelo y serializador

--- core/submission/views.py
+++ core/submission/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from API import SuperApiView
+from .serializer import SubmissionSerializer
+from .models import Submission
 # Create your views here.
+class SubmissionView(SuperApiView):
+    model = Submission
+    serializer_class = SubmissionSerializer
+    filter_fields = ["id"]
