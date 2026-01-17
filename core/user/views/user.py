fe96aff345578568f3615dc71922fd8965403f04
Sebas16608 <asebasrr444@gmail.com>
2026-01-17 17:13:36 -0600

    Definiendo la busqueda por filtros en la view de User

--- core/user/views/user.py
+++ core/user/views/user.py
@@ -7,7 +7,10 @@ from ..serializers.user import UserSerializer
 def notexist():
     return {"error": "Los datos no fueron encontrados"}
 
+
 class UserView(APIView):
+    filter_fields = ["username", "first_name", "last_name"]
+
     def get(self, request, pk=None):
         if pk:
             try:
@@ -16,7 +19,20 @@ class UserView(APIView):
                 return Response(serializer.data, status=status.HTTP_200_OK)
             except User.DoesNotExist:
                 return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
+
+        # Filtros
+        filters = {}
+        for field in self.filter_fields:
+            value = request.query_params.get(field)
+            if value is not None:
+                filters[field + "__icontains"] = value  # para búsqueda parcial
+
+        if filters:
+            queryset = User.objects.filter(**filters)
+            if not queryset.exists():
+                return Response(notexist(), status=status.HTTP_404_NOT_FOUND)
         else:
-            user = User.objects.all()
-            serializer = UserSerializer(user, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            queryset = User.objects.all()
+
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
