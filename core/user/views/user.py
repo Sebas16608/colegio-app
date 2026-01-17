504c062b978f14ad4b1f42d14013503ddab4b847
Sebas16608 <asebasrr444@gmail.com>
2026-01-17 17:00:18 -0600

    Definiendo el metodo GET de la view de User

---
+++ core/user/views/user.py
@@ -0,0 +1,22 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from ..models import User
+from ..serializers.user import UserSerializer
+
+def notexist():
+    return {"error": "Los datos no fueron encontrados"}
+
+class UserView(APIView):
+    def get(self, request, pk=None):
+        if pk:
+            try:
+                user = User.objects.get(pk=pk)
+                serializer = UserSerializer(user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                return Response()
+        else:
+            user = User.objects.all()
+            serializer = UserSerializer(user, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
