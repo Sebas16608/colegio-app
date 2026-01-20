438da9f7f77017e922004ba23e8034847a478eef
Sebas16608 <asebasrr444@gmail.com>
2026-01-20 12:01:28 -0600

    Arreglando el endpoint para ver listar a todos los usuarios

--- core/user/urls.py
+++ core/user/urls.py
@@ -4,6 +4,6 @@ from .views import LoginView, RegisterView, UserView
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
-    path("user", UserView.as_view(), name="user"),
+    path("user/", UserView.as_view(), name="user"),
     path("user/<int:pk>/", UserView.as_view(), name="User-detail")
 ]
