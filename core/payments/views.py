4b5c4d38b353eb926ed41f94d1bf0d7572297c39
Sebas16608 <asebasrr444@gmail.com>
2026-01-19 22:09:09 -0600

    definiendo la API de payments

--- core/payments/views.py
+++ core/payments/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from API import SuperApiView
+from .serializer import PaymentSerializer
+from .models import Payment
 # Create your views here.
+class PaymentView(SuperApiView):
+    model = Payment
+    serializer_class = Payment
+    filter_fields = ["student", "month", "status", "validated_by"]
+
+
