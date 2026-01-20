c59e50ebd9be6eb65be66d0e48540542037e03a3
Sebas16608 <asebasrr444@gmail.com>
2026-01-20 11:43:06 -0600

    Definiendo el modelo de conversations

---
+++ services/chat/src/models/conversation.ts
@@ -0,0 +1,27 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "config/database";
+
+class Conversations extends Model{
+    declare id?: number;
+    declare type: string;
+}
+
+Conversations.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},
+{
+    sequelize,
+    modelName: "Conversation",
+    freezeTableName: true
+}
+);
+
+export default Conversations;
