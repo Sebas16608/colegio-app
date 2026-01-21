762b905a3bc2b3abc1daa149a0d30f8829a31e6f
Sebas16608 <asebasrr444@gmail.com>
2026-01-20 21:33:43 -0600

    feat: add Sequelize sync with Conversation, Participants and Message models + relations

--- services/chat/src/server.ts
+++ services/chat/src/server.ts
@@ -1,17 +1,26 @@
 import app from "./app";
-import sequelize from "config/database";
+import sequelize from "./config/database";
 
 // Importacion de modelos
-import Conversations from "models/conversation";
-import ConversationParticipant from "models/conversationParticipant";
-import Message from "models/message";
+import Conversations from "./models/conversation";
+import ConversationParticipant from "./models/conversationParticipant";
+import Message from "./models/message";
 
 const port: number = 3000;
+
+// Relaciones
+// Conversacion <-> Participante
+Conversations.hasMany(ConversationParticipant, { foreignKey: "conversation_id" });
+ConversationParticipant.belongsTo(Conversations, { foreignKey: "conversation_id" });
+
+// Conversacion <-> Mensaje
+Conversations.hasMany(Message, { foreignKey: "conversation_id" });
+Message.belongsTo(Conversations, { foreignKey: "conversation_id" });
 
 (async () => {
     try {
         await sequelize.authenticate();
-        await sequelize.sync();
+        await sequelize.sync({ alter: true });
 
         console.log("DB sincronizada");
 
@@ -21,4 +30,4 @@ const port: number = 3000;
     } catch (error){
         console.log(`Error al iniciar el servidor ${error}`);
     };
-});
+})();
