4a1c3e3946f95f50a6c898b551847d0bb5407322
Sebas16608 <asebasrr444@gmail.com>
2026-01-20 21:09:02 -0600

    Definiendo el modelo de message

--- services/chat/src/models/message.ts
+++ services/chat/src/models/message.ts
@@ -4,10 +4,46 @@ import sequelize from "config/database";
 class Message extends Model{
     declare id?: number;
     declare conversation_id: number;
-    declare sender_id: number;
     declare content: string;
     declare message_type: string;
     declare is_read: boolean;
 };
 
-
+Message.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    conversation_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Conversation",
+            key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    message_type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "Text"
+    },
+    is_read: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+    },
+},
+{
+    sequelize,
+    modelName: "Message",
+    freezeTableName: true,
+    underscored: true
+}
+);
