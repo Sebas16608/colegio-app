4dc7e88590fcdb89196e4879cfbb45f7cddb4497
Sebas16608 <asebasrr444@gmail.com>
2026-01-21 13:58:59 -0600

    chore: Creating the POST method for conversations-participan

--- services/chat/src/controllers/conversationParticipant-controller.ts
+++ services/chat/src/controllers/conversationParticipant-controller.ts
@@ -5,7 +5,7 @@ const getAllConversationParticipant = async (req: Request, res: Response) => {
        try {
             const participant = await ConversationParticipant.findAll()
             res.status(200).json(participant);
-       } catch (error) {
+    } catch (error) {
             res.status(500).json({ error: `Internal Server Error ${error}`});
-       }
+    }
 }
