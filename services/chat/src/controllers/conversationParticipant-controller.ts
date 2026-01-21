b42f82e55c83f190deae7ba55022323fd8a083e6
Sebas16608 <asebasrr444@gmail.com>
2026-01-21 14:14:25 -0600

    chore: Creating the GET  method for conversations-participant add two';'

--- services/chat/src/controllers/conversationParticipant-controller.ts
+++ services/chat/src/controllers/conversationParticipant-controller.ts
@@ -2,10 +2,23 @@ import ConversationParticipant from "models/conversationParticipant";
 import { Request, Response } from "express";
 
 const getAllConversationParticipant = async (req: Request, res: Response) => {
-       try {
-            const participant = await ConversationParticipant.findAll()
+    try {
+            const participant = await ConversationParticipant.findAll();
             res.status(200).json(participant);
     } catch (error) {
             res.status(500).json({ error: `Internal Server Error ${error}`});
+    }
+}
+
+const getParticipantById = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params);
+        const participant = await ConversationParticipant.findByPk(id)
+        
+        if (!participant)  return res.status(404).json({ error: "Not Found" });
+        
+        res.status(200).json(participant);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
     }
 }
