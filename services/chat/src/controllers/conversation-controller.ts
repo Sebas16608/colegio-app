955446bf9a8dce2ebb9a9b20a2e1da6b62e96231
Sebas16608 <asebasrr444@gmail.com>
2026-01-20 22:31:15 -0600

    chore: Creating the GET method for conversations

---
+++ services/chat/src/controllers/conversation-controller.ts
@@ -0,0 +1,34 @@
+import Conversations from "models/conversation";
+import { Request, Response } from "express";
+
+const getAllConversations = async (req: Request, res: Response) => {
+    try {
+        const conversation = await Conversations.findAll();
+        res.status(200).json(conversation);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    };
+};
+
+const getConversationById = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params);
+        
+        const conversation = await Conversations.findByPk(id);
+        
+        if (!conversation) {
+            return res.status(404).json({ 
+                message: 'Conversación no encontrada' 
+            });
+        }
+        
+        return res.status(200).json(conversation);
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ 
+            message: 'Error del servidor' 
+        });
+    };
+};
+
