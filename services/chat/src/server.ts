fa15d4fb3116edbd26bff5930c6943f5ce629657
Sebas16608 <asebasrr444@gmail.com>
2026-01-20 21:16:04 -0600

    Sincronizando la DB y creando la inicializacion definitiva del servidor

--- services/chat/src/server.ts
+++ services/chat/src/server.ts
@@ -1,6 +1,24 @@
 import app from "./app";
+import sequelize from "config/database";
+
+// Importacion de modelos
+import Conversations from "models/conversation";
+import ConversationParticipant from "models/conversationParticipant";
+import Message from "models/message";
+
 const port: number = 3000;
 
-app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+(async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+
+        console.log("DB sincronizada");
+
+        app.listen(port, () => {
+            console.log(`Servidor corriendo en http://localhost:${port}`);
+        });
+    } catch (error){
+        console.log(`Error al iniciar el servidor ${error}`);
+    };
 });
