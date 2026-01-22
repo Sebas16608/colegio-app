fdac5297f7099d1c9c1a0fd8369b5cd6790280b3
Sebas16608 <asebasrr444@gmail.com>
2026-01-22 16:56:38 -0600

    chore: Creating the POST and PUT  method for conversations-participant

--- services/chat/src/controllers/conversationParticipant-controller.ts
+++ services/chat/src/controllers/conversationParticipant-controller.ts
@@ -22,3 +22,31 @@ const getParticipantById = async (req: Request, res: Response) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+const postConversationParticipant = async (req: Request, res: Response) => {
+    try {
+        const { conversation_id, user_id } = req.body;
+
+        const participant = await ConversationParticipant.create({ conversation_id, user_id });
+        return res.status(201).json(participant);
+    } catch (err) {
+        return res.status(400).json({ error: "Bad Request" });
+    }
+}
+
+const putConversationParticipant = async (req: Request, res: Response) => {
+    try {
+        const id = Number(req.params);
+        const participant = await ConversationParticipant.findByPk(id);
+        if (!participant) return res.status(404).json({ error: "Not found" });
+
+        const { conversation_id, user_id } = req.body;
+        participant.conversation_id = conversation_id ?? participant.conversation_id;
+        participant.user_id = user_id ?? user_id;
+
+        await participant.save();
+        return res.json(participant);
+    } catch (error) {
+       return res.status(500).json({ error: `Internal Server Error ${error}`});
+    }
+}
