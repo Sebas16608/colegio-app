6d940d54d57515332869fb9d77e2929682da6158
Sebas16608 <asebasrr444@gmail.com>
2026-01-24 11:41:48 -0600

    chore: Creating the routers for conversations

---
+++ services/chat/src/routes/conversation-route.ts
@@ -0,0 +1,10 @@
+import { getAllConversations, getConversationById, postConversation, putConversation, deleteConversation} from "../controllers/conversation-controller";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", getAllConversations);
+router.get("/:id", getConversationById);
+router.post("/", postConversation);
+router.put("/:id", putConversation);
+router.delete("/:id", deleteConversation);
